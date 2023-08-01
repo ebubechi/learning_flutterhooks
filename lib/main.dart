c189cb457bb2e5a0f8c9dc25fc9ea3db25253fe3
ebubechi <emmanuelsonzico@gmail.com>
2023-08-01 14:37:09 +0100

    useStreamController

--- lib/main.dart
+++ lib/main.dart
@@ -52,84 +52,38 @@ const uri =
     'https://www.thewowstyle.com/wp-content/uploads/2015/01/images-of-nature-4.jpg';
 const imageHeight = 300.0;
 
-extension Normalize on num {
-  num normalized(
-    num selfRangeMin,
-    num selfRangeMax, [
-    num normalizedRangeMin = 0.0,
-    num normalizedRangeMax = 1.0,
-  ]) =>
-      (normalizedRangeMax - normalizedRangeMin) *
-          ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) +
-      normalizedRangeMin;
-}
-
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final opacity = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1.0,
-      lowerBound: 0.0,
-      upperBound: 1.0,
-    );
-    final size = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1.0,
-      lowerBound: 0.0,
-      upperBound: 1.0,
-    );
-
-    final controller = useScrollController();
-
-    useEffect(() {
-      controller.addListener(() {
-        final newOpacity = max(imageHeight - controller.offset, 0.0);
-        final normalized = newOpacity.normalized(0.0, imageHeight).toDouble();
-        opacity.value = normalized;
-        size.value = normalized;
-      });
-      return null;
-    }, [controller]);
-
+    late final StreamController<double> controller;
+    controller = useStreamController<double>(onListen: () {
+      controller.sink.add(0.0);
+    });
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hey There!'),
-        // title: Text(dataTime.data ?? 'Hey There!'),
-      ),
-      body: Column(
-        children: [
-          SizeTransition(
-            sizeFactor: size,
-            axis: Axis.vertical,
-            axisAlignment: -1.0,
-            child: FadeTransition(
-              opacity: opacity,
-              child: Image.network(
-                uri,
-                height: imageHeight,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                controller: controller,
-                itemCount: 100,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('Person ${index + 1}'),
-                  );
-                },
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Hey There!'),
+        ),
+        body: StreamBuilder<double>(
+            stream: controller.stream,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const CircularProgressIndicator();
+              } else {
+                final rotation = snapshot.data ?? 0.0;
+                return GestureDetector(
+                  onTap: () {
+                    controller.sink.add(rotation + 10.0);
+                  },
+                  child: RotationTransition(
+                    turns: AlwaysStoppedAnimation(rotation / 7),
+                    child: Center(
+                      child: Image.network(uri),
+                    ),
+                  ),
+                );
+              }
+            }));
   }
 }
