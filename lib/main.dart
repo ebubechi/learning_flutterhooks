2bf095839fc5a7c9c0dae947533a137a65f0159b
ebubechi <emmanuelsonzico@gmail.com>
2023-08-01 11:23:52 +0100

    useFuture and useMemoized

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> conmpactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(transform ?? (e) => e).where((e) => e != null).cast();
+}
+
+// void testIt() {
+//   final values = [1, 2, null, 3];
+//   final nonNullValues = values.conmpactMap();
+//   log(nonNullValues.toString());
+// }
 
 void main() {
   runApp(MaterialApp(
@@ -15,39 +31,38 @@ void main() {
 //       (_) => DateTime.now().toIso8601String(),
 //     );
 
+const uri =
+    'https://www.thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg';
+
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // final dataTime = useStream(getTime());
-    final controller = useTextEditingController();
-    final text = useState('');
+    /// * This is caching the api call
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(uri))
+        .load(uri)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
 
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
-    );
-
+    final snapshot = useFuture(future);
+    // final image = useFuture(NetworkAssetBundle(Uri.parse(uri))
+    //     .load(uri)
+    //     .then((data) => data.buffer.asUint8List())
+    //     .then((data) => Image.memory(data)));
+    // testIt();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hey There!'),
         // title: Text(dataTime.data ?? 'Hey There!'),
       ),
       body: Column(
-        children: [
-          TextField(
-            controller: controller,
+          children: [
+        snapshot.data,
+      ].conmpactMap().toList()
+          //   image.hasData ? image.data! : null,
+          // ].conmpactMap().toList(),
           ),
-          const SizedBox(height: 20.0,),
-          Text('You typed ${text.value}'),
-        ],
-      ),
     );
   }
 }
