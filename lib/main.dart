801871238465fc244a0829a624dfc106984ee89f
ebubechi <emmanuelsonzico@gmail.com>
2023-08-02 00:54:53 +0100

    useReducer

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
 import 'dart:async';
-import 'dart:developer';
 import 'dart:math';
+// import 'dart:developer';
+// import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> conmpactMap<E>([
-    E? Function(T?)? transform,
-  ]) =>
-      map(transform ?? (e) => e).where((e) => e != null).cast();
-}
-
-// void testIt() {
-//   final values = [1, 2, null, 3];
-//   final nonNullValues = values.conmpactMap();
-//   log(nonNullValues.toString());
-// }
 
 void main() {
   runApp(MaterialApp(
@@ -31,59 +19,183 @@ void main() {
   ));
 }
 
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription sub;
-  CountDown({required int from}) : super(from) {
-    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
-        .takeWhile((value) => value >= 0)
-        .listen((value) {
-      this.value = value;
-    });
-  }
-
-  @override
-  void dispose() {
-    sub.cancel();
-    super.dispose();
-  }
-}
-
 const uri =
     'https://www.thewowstyle.com/wp-content/uploads/2015/01/images-of-nature-4.jpg';
-const imageHeight = 300.0;
+
+enum Action {
+  rotateRight,
+  rotateLeft,
+  moreVisible,
+  lessVisible,
+}
+
+@immutable
+class State {
+  final double rotationDeg;
+  final double alpha;
+
+  const State({
+    required this.rotationDeg,
+    required this.alpha,
+  });
+
+  const State.zero()
+      : rotationDeg = 0.0,
+        alpha = 1.0;
+
+  State rotateRight() => State(
+        rotationDeg: rotationDeg + 10.0,
+        alpha: alpha,
+      );
+
+  State rotateLeft() => State(
+        rotationDeg: rotationDeg - 10.0,
+        alpha: alpha,
+      );
+
+  State increaseAlpha() => State(
+        rotationDeg: rotationDeg,
+        alpha: min(alpha + 0.1, 1.0),
+      );
+
+  State decreaseAlpha() => State(
+        rotationDeg: rotationDeg,
+        alpha: max(alpha - 0.1, 0.0),
+      );
+}
+
+State reducer(State oldState, Action? action) {
+  switch (action) {
+    case Action.rotateLeft:
+      return oldState.rotateLeft();
+    case Action.rotateRight:
+      return oldState.rotateRight();
+    case Action.moreVisible:
+      return oldState.increaseAlpha();
+    case Action.lessVisible:
+      return oldState.decreaseAlpha();
+    case null:
+      return oldState;
+  }
+}
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    late final StreamController<double> controller;
-    controller = useStreamController<double>(onListen: () {
-      controller.sink.add(0.0);
-    });
+    final store = useReducer<State, Action?>(
+      reducer,
+      initialState: const State.zero(),
+      initialAction: null,
+    );
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Hey There!'),
-        ),
-        body: StreamBuilder<double>(
-            stream: controller.stream,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const CircularProgressIndicator();
-              } else {
-                final rotation = snapshot.data ?? 0.0;
-                return GestureDetector(
-                  onTap: () {
-                    controller.sink.add(rotation + 10.0);
-                  },
-                  child: RotationTransition(
-                    turns: AlwaysStoppedAnimation(rotation / 7),
-                    child: Center(
-                      child: Image.network(uri),
-                    ),
-                  ),
-                );
-              }
-            }));
+      appBar: AppBar(
+        title: const Text('Hey There!'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              RotateLeftButton(store: store),
+              RotateRightButton(store: store),
+              DecreaseAlphaButton(store: store),
+              IncreaseAlphaButton(store: store),
+            ],
+          ),
+          const SizedBox(
+            height: 120.0,
+          ),
+          Opacity(
+            opacity: store.state.alpha,
+            child: RotationTransition(
+                turns: AlwaysStoppedAnimation(
+                  store.state.rotationDeg / 360.0,
+                ),
+                child: Image.network(uri)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IncreaseAlphaButton extends StatelessWidget {
+  const IncreaseAlphaButton({
+    super.key,
+    required this.store,
+  });
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+          onPressed: () {
+            store.dispatch(Action.moreVisible);
+          },
+          child: const Text('Increase Alpha')),
+    );
+  }
+}
+
+class DecreaseAlphaButton extends StatelessWidget {
+  const DecreaseAlphaButton({
+    super.key,
+    required this.store,
+  });
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+          onPressed: () {
+            store.dispatch(Action.lessVisible);
+          },
+          child: const Text('Decrease Alpha')),
+    );
+  }
+}
+
+class RotateRightButton extends StatelessWidget {
+  const RotateRightButton({
+    super.key,
+    required this.store,
+  });
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+          onPressed: () {
+            store.dispatch(Action.rotateRight);
+          },
+          child: const Text('Rotate Right')),
+    );
+  }
+}
+
+class RotateLeftButton extends StatelessWidget {
+  const RotateLeftButton({
+    super.key,
+    required this.store,
+  });
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+          onPressed: () {
+            store.dispatch(Action.rotateLeft);
+          },
+          child: const Text('Rotate Left')),
+    );
   }
 }
