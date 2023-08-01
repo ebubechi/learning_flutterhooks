83b9341cd7b152cb071a7f831f101dfff6ee9a41
ebubechi <emmanuelsonzico@gmail.com>
2023-08-01 12:32:49 +0100

    useListenable and useMemoized

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -21,48 +22,49 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Flutter Demo',
-    theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.amber),
+    theme: ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.amber,
+    ),
     home: const HomePage(),
   ));
 }
 
-// Stream<String> getTime() => Stream.periodic(
-//       const Duration(seconds: 1),
-//       (_) => DateTime.now().toIso8601String(),
-//     );
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
+        .takeWhile((value) => value >= 0)
+        .listen((value) {
+      this.value = value;
+    });
+  }
 
-const uri =
-    'https://www.thewowstyle.com/wp-content/uploads/2015/01/nature-images..jpg';
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    /// * This is caching the api call
-    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(uri))
-        .load(uri)
-        .then((data) => data.buffer.asUint8List())
-        .then((data) => Image.memory(data)));
-
-    final snapshot = useFuture(future);
-    // final image = useFuture(NetworkAssetBundle(Uri.parse(uri))
-    //     .load(uri)
-    //     .then((data) => data.buffer.asUint8List())
-    //     .then((data) => Image.memory(data)));
-    // testIt();
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hey There!'),
         // title: Text(dataTime.data ?? 'Hey There!'),
       ),
-      body: Column(
-          children: [
-        snapshot.data,
-      ].conmpactMap().toList()
-          //   image.hasData ? image.data! : null,
-          // ].conmpactMap().toList(),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Text(
+          notifier.value.toString(),
+        ),
+      ),
     );
   }
 }
